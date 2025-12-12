69bae1acb5054fcfc2a6482a72300f770666cf25
Federico Marra <marrafederico@proton.me>
2025-12-12 23:40:18 +0100

    (style): add loading spinner to auth form button

--- src/app/core/auth/auth.component.ts
+++ src/app/core/auth/auth.component.ts
@@ -11,11 +11,12 @@ import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { LoginRequest, RegisterRequest } from '../../shared/models/authModel';
 import { MessageModule } from 'primeng/message';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
 
 
 @Component({
   selector: 'app-auth',
-  imports: [InputGroupAddonModule, MessageModule, CardModule, PasswordModule, InputTextModule, InputGroupModule, FloatLabelModule, ButtonModule, RouterLink, ReactiveFormsModule],
+  imports: [InputGroupAddonModule, ProgressSpinnerModule, MessageModule, CardModule, PasswordModule, InputTextModule, InputGroupModule, FloatLabelModule, ButtonModule, RouterLink, ReactiveFormsModule],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.css',
 })
@@ -28,6 +29,7 @@ export class AuthComponent implements OnInit {
   isLoginPage: boolean = false;
   authForm!: FormGroup;
   error = signal<string | null>(null);
+  isLoading = signal<boolean>(false);
 
   ngOnInit(): void {
     const url = this.activatedRoute.url.subscribe({
@@ -56,6 +58,7 @@ export class AuthComponent implements OnInit {
   }
 
   onSubmit() {
+    this.isLoading.set(true);
     if (this.isLoginPage && this.authForm.valid) {
       const userData: LoginRequest = this.authForm.value;
       this.authService.login(userData).subscribe({
@@ -64,9 +67,11 @@ export class AuthComponent implements OnInit {
           this.authService.changeAuthState();
           this.error.set(null);
           this.authForm.reset();
+          this.isLoading.set(false);
           this.router.navigate(['/']);
         },
         error: (err) => {
+          this.isLoading.set(false);
           this.error.set(err.error.detail);
         }
       });
@@ -76,9 +81,11 @@ export class AuthComponent implements OnInit {
         next: () => {
           this.error.set(null);
           this.authForm.reset();
+          this.isLoading.set(false);
           this.router.navigate(['/auth/login']);
         },
         error: (err) => {
+          this.isLoading.set(false);
           this.error.set(err.error.detail);
         }
       });
