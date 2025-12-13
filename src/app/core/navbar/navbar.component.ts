6405fcead002fec10010d74f0bb09fc57bfd3831
Federico Marra <marrafederico@proton.me>
2025-12-13 18:10:47 +0100

    style: add popup menu

--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -1,15 +1,33 @@
-import { Component, inject } from '@angular/core';
-import { ButtonModule } from 'primeng/button';
-import { RouterLink } from "@angular/router";
+import { Component, inject, OnInit } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../shared/services/auth.service';
+
+import { MenuModule } from 'primeng/menu';
+import { MenuItem } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-navbar',
-  imports: [ButtonModule, RouterLink],
+  imports: [ButtonModule, RouterLink, MenuModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
   public authService = inject(AuthService);
+  private router = inject(Router);
 
+  menuItems!: MenuItem[];
+
+  ngOnInit(): void {
+    this.menuItems = [
+      {
+        label: 'Settings',
+        routerLink: '/profile',
+      },
+      {
+        label: 'Logout',
+        routerLink: '/auth/logout',
+      },
+    ];
+  }
 }
