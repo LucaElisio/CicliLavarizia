4e572a084bfa90fc619d0939b0ef7ff52b3f7825
Federico Marra <marrafederico@proton.me>
2025-12-19 17:32:24 +0100

    fix: correct address property reference and update formatting in customer model

--- src/app/shared/models/customerModel.ts
+++ src/app/shared/models/customerModel.ts
@@ -1,28 +1,27 @@
 export interface CustomerInfoRequest {
-    customerId: number,
-    firstName: string,
-    middleName: string,
-    lastName: string,
-    suffix: string,
-    companyName: string,
-    salesPerson: string
-    phone: string,
-    customerAddresses: CustomerAddress[]
+  customerId: number;
+  firstName: string;
+  middleName: string;
+  lastName: string;
+  suffix: string;
+  companyName: string;
+  salesPerson: string;
+  phone: string;
+  customerAddresses: CustomerAddress[];
 }
 
 interface CustomerAddress {
-    customerId: number,
-    addressId: number,
-    addressType: string,
-    Address: Address
+  customerId: number;
+  addressId: number;
+  addressType: string;
+  address: Address;
 }
 
 interface Address {
-    addressId: number,
-    addressLine1: string
-    city: string
-    stateProvince: string
-    countryRegion: string
-    postalCode: string
-
+  addressId: number;
+  addressLine1: string;
+  city: string;
+  stateProvince: string;
+  countryRegion: string;
+  postalCode: string;
 }
